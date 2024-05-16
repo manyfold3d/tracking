a33cd6878f307adb93f7148971285abd6c492c12
James Smith <james@floppy.org.uk>
2024-05-16 20:42:59 +0100

    cache stats

--- tracking.rb
+++ tracking.rb
@@ -1,6 +1,7 @@
 require 'grape'
 
 $data = {}
+$stats = nil
 
 module Tracking
   class API < Grape::API
@@ -11,6 +12,15 @@ module Tracking
     helpers do
       def store(id, version)
         $data[id] = version.merge(time: DateTime.now)
+        $stats = nil
+      end
+
+      def calculate_stats
+        recent = $data.select{ |k,v| v[:time] >= (Date.today - 7) }
+        {
+          instances: recent.size,
+          versions: recent.group_by{ |k,v| v["app"] }.map{|k,v| [k, v.length]}.to_h
+        }
       end
     end
 
@@ -33,11 +43,7 @@ module Tracking
 
     desc "Get aggregated usage stats"
     get :stats do
-      recent = $data.select{ |k,v| v[:time] >= (Date.today - 7) }
-      {
-        instances: recent.size,
-        versions: recent.group_by{ |k,v| v["app"] }.map{|k,v| [k, v.length]}.to_h
-      }
+      $stats ||= calculate_stats
     end
   end
 end
